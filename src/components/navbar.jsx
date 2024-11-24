9cc9147cb69a889eb1b85d923f372e6f1319abfe
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-11-24 15:47:01 -0800

    Fix spacing, add pages

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+// import {Link} from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top"> {/* Apply the fixed-top class here */}
             <div className="container">
-                <a className="navbar-brand" href="#">Winter Wonderland</a>
+                <a className="navbar-brand" href="/">Winter Wonderland</a>
 
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
--- src/components/welcomebutton.jsx
+++ src/components/welcomebutton.jsx
@@ -6,7 +6,7 @@ import {Link} from 'react-router-dom';
 const Welcomebutton = () => {
     return (
         <Link to="/welcome" className="welcome-button">
-             <button>Welcome</button>
+             <button>Enter</button>
         </Link>
     )
 }
