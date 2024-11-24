fdf609f48ef1d688743ad366b361acfdec7064c0
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-11-24 12:17:34 -0800

    Button code and links

--- src/components/welcomebutton.jsx
+++ src/components/welcomebutton.jsx
@@ -2,15 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 
-// const Welcomebutton = () => {
-//     return (
-
-// <div className='form-grid'>
-// <button type="submit" class="btn btn-primary btn-lg" id='button'>Submit</button>
-// {/* {submissionStatus && <p>{submissionStatus}</p>} */}
-// </div>
-
-
 
 const Welcomebutton = () => {
     return (
@@ -20,3 +11,13 @@ const Welcomebutton = () => {
 }
 
 export default Welcomebutton;
+
+
+
+// const Welcomebutton = () => {
+//     return (
+
+// <div className='form-grid'>
+// <button type="submit" class="btn btn-primary btn-lg" id='button'>Submit</button>
+// {/* {submissionStatus && <p>{submissionStatus}</p>} */}
+// </div>
