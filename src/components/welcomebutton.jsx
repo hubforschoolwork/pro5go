e7c1c70607045bbbccbab0cce84055e838f36a2b
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-11-29 14:37:23 -0800

    button code fixes

--- src/components/welcomebutton.jsx
+++ src/components/welcomebutton.jsx
@@ -5,20 +5,10 @@ import {Link} from 'react-router-dom';
 
 const Welcomebutton = () => {
     return (
-        <Link to="/welcome" className="welcome-button">
+        <Link to="/guestarea" className="welcome-button">
              <button type="submit">Enter</button>
         </Link>
     )
 }
 
 export default Welcomebutton;
-
-
-
-// const Welcomebutton = () => {
-//     return (
-
-// <div className='form-grid'>
-// <button type="submit" class="btn btn-primary btn-lg" id='button'>Submit</button>
-// {/* {submissionStatus && <p>{submissionStatus}</p>} */}
-// </div>
