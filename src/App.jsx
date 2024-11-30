ad407235e31c01d6411f5f3682dd8a2bf34cdd9c
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-11-30 15:47:15 -0800

    adjust code

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,10 @@ import Registeruser from './pages/register.jsx';
 import Welcome from './pages/welcome.jsx';
 import Guest from './pages/guestarea.jsx';
 import Testlink from './pages/testlink.jsx';
-import Cookingb from './pages/cooking.jsx';
+// import Cookingb from './pages/cooking.jsx';
 // import Indoor from './pages/indoor.jsx';
 // import Outdoor from './pages/outdoor.jsx';
-import Giftshopping from './pages/giftshopping.jsx';
+// import Giftshopping from './pages/giftshopping.jsx';
 // import Sports from './pages/sports.jsx';
 
 
@@ -33,16 +33,16 @@ function App() {
           <Route path="/guestarea" element={<Guest/>} />
           <Route path="/testlink" element={<Testlink/>} />
 
-          <Route path="/indoor" element={<Indoor/>} />
+          {/* <Route path="/indoor" element={<Indoor/>} />
           <Route path="/outdoor" element={<Outdoor/>} />
           <Route path="/giftshopping" element={<Giftshopping/>} />
 
 
-          <Route path="/sports" element={<Sports/>} />
+          <Route path="/sports" element={<Sports />} />
 
 
-          <Route path="/cooking" element={<Cookingb/>} />
-          <Route path="/cooking" element={<cooking/>} />
+          <Route path="/cooking" element={<Cookingb />} />
+          <Route path="/cooking" element={<cooking />} /> */}
 
         </Routes>
 </div>
---
+++ src/pages/outdoor.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Dashboard from '../components/dashboardc.jsx';
+import './guestarea.css'
+
+function Outdoor() {
+    return (
+
+        <>
+        <Dashboard /><div className="form-container">
+            <div>
+                <h3>Welcome Outdoor Visitor</h3>
+
+
+                <h3>Hello Winter Fan!</h3>
+            </div>
+        </div>
+        </>
+
+    );
+}
+
+export default Outdoor;
