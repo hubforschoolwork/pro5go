79afbad80228d9e37457bad2be09967a55cf1897
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-11-30 16:13:53 -0800

    add component

---
+++ src/components/cookinghub.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Dashboard from '../components/dashboardc.jsx';
+import './cooking.css'
+
+function Cookingb() {
+    return (
+        <>
+        <Dashboard />
+
+        <div className="form-container">
+            <h3>Winter Cooking</h3>
+        </div>
+
+        <div>
+                <h2>Here's a Cooking Adventure for Winter</h2>
+                <h4>You'll find recipes, tips, and inspiration for your favorite outdoor activities.</h4>
+            </div>
+        </>
+    );
+}
+
+export default Cookingb;
