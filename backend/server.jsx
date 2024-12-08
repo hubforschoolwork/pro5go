e49bac2eed973e038fad631009f7a1e0ddb1d7a4
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-12-08 15:05:29 -0800

    Add code for backend

---
+++ backend/server.jsx
@@ -0,0 +1,85 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const filePath = "./demo/users.json";
+
+// Retrieve users
+app.get("/api/users", (req, res) => {
+    const users = JSON.parse(fs.readFileSync(filePath));
+    res.json(users);
+});
+
+// Add a new user
+app.post("/api/register", (req, res) => {
+    const newUser = req.body;
+
+    const users = JSON.parse(fs.readFileSync(filePath));
+    users.push(newUser);
+
+    fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+    res.json({ message: "User added successfully!" });
+});
+
+app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express');
+// const mysql = require('mysql');
+// const cors = require('cors');
+
+// const app = express();
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // MySQL Database Connection
+// const db = mysql.createConnection({
+//     host: 'localhost', // Change to your MySQL host
+//     user: 'root',      // Change to your MySQL username
+//     password: '',      // Change to your MySQL password
+//     database: 'test',  // Change to your database name
+// });
+
+// db.connect((err) => {
+//     if (err) {
+//         console.error('Database connection failed:', err.stack);
+//         return;
+//     }
+//     console.log('Connected to MySQL database.');
+// });
+
+// // Example Route
+// app.get('/api/test', (req, res) => {
+//     db.query('SELECT * FROM example_table', (err, results) => {
+//         if (err) {
+//             res.status(500).json({ error: err });
+//         } else {
+//             res.json(results);
+//         }
+//     });
+// });
+
+// // Start the server
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+// });
