ec9d20a81b114b11cf94b18f9985ce17fe216204
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-12-09 15:44:43 -0800

    backend coding

--- backend/demo/users.js
+++ backend/demo/users.js
@@ -1,19 +1,14 @@
-// import fs from "fs";
-
-// const users = JSON.parse(fs.readFileSync("./demo/users.js"));
-
-
 import fs from "fs";
 
-const filePath = "./users.json";
+const filePath = "./users.json"; // Path to your JSON file
 
-// Load mock data from JSON file
+// Function to read users from the JSON file
 export const getUsers = () => {
-  const data = fs.readFileSync(filePath);
-  return JSON.parse(data);
+  const data = fs.readFileSync(filePath); // Read the file
+  return JSON.parse(data); // Parse the JSON and return the user data
 };
 
-// Save updated data to JSON file
+// Function to write users to the JSON file
 export const saveUsers = (users) => {
-  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2)); // Write the updated user list
 };
--- backend/server.jsx
+++ backend/server.jsx
@@ -1,114 +1,28 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { getUsers, saveUsers } from "./users.js";
+import { getUsers, saveUsers } from "./users.js"; // Import the functions
+
+
 
 const app = express();
 app.use(bodyParser.json());
 
-// Get all users
+// Endpoint to get all users
 app.get("/api/users", (req, res) => {
-  const users = getUsers();
+  const users = getUsers(); // Use the getUsers function
   res.json(users);
 });
 
-// Add a new user
+// Endpoint to register a new user
 app.post("/api/register", (req, res) => {
   const newUser = req.body;
-  const users = getUsers();
-  users.push(newUser);
-  saveUsers(users);
-  res.json({ message: "User added successfully!" });
+  const users = getUsers(); // Get the current users
+
+  newUser.id = users.length + 1; // Assign a new ID
+  users.push(newUser); // Add the new user
+
+  saveUsers(users); // Save the updated user list
+  res.json({ message: "User registered successfully!", user: newUser });
 });
 
 app.listen(5000, () => console.log("Server running on http://localhost:5000"));
-
-
-
-
-// December 8, 2024********************************************************
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const fs = require("fs");
-
-// const app = express();
-
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// const filePath = "./demo/users.json";
-
-// // Retrieve users
-// app.get("/api/users", (req, res) => {
-//     const users = JSON.parse(fs.readFileSync(filePath));
-//     res.json(users);
-// });
-
-// // Add a new user
-// app.post("/api/register", (req, res) => {
-//     const newUser = req.body;
-
-//     const users = JSON.parse(fs.readFileSync(filePath));
-//     users.push(newUser);
-
-//     fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
-//     res.json({ message: "User added successfully!" });
-// });
-
-// app.listen(5000, () => console.log("Backend running on http://localhost:5000"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const express = require('express');
-// const mysql = require('mysql');
-// const cors = require('cors');
-
-// const app = express();
-
-// // Middleware
-// app.use(cors());
-// app.use(express.json());
-
-// // MySQL Database Connection
-// const db = mysql.createConnection({
-//     host: 'localhost', // Change to your MySQL host
-//     user: 'root',      // Change to your MySQL username
-//     password: '',      // Change to your MySQL password
-//     database: 'test',  // Change to your database name
-// });
-
-// db.connect((err) => {
-//     if (err) {
-//         console.error('Database connection failed:', err.stack);
-//         return;
-//     }
-//     console.log('Connected to MySQL database.');
-// });
-
-// // Example Route
-// app.get('/api/test', (req, res) => {
-//     db.query('SELECT * FROM example_table', (err, results) => {
-//         if (err) {
-//             res.status(500).json({ error: err });
-//         } else {
-//             res.json(results);
-//         }
-//     });
-// });
-
-// // Start the server
-// const PORT = 5000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-// });
