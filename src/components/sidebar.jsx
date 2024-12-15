5a76184e7c003f08ab67909aede2915deac9b9ec
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-12-15 14:45:43 -0800

    change background image and css

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -3,7 +3,7 @@ import '../pages/sidebar.css'; // Import custom styles (optional)
 
 const Sidebar = () => {
     return (
-        <div className="sidebar d-flex flex-column bg-dark text-white vh-100">
+        <div className="sidebar d-flex flex-column bg-primary bg-opacity-50 text-white vh-100">
             <h2 className="p-3">Dashboard</h2>
             <ul className="nav flex-column">
 
