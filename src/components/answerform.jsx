bd7693279d17164e2222d796e966f11f2ce484fb
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-12-15 15:48:32 -0800

    edit code

--- src/components/answerform.jsx
+++ src/components/answerform.jsx
@@ -8,7 +8,12 @@ function Answerit() {
             <h2>Answer a Question</h2>
             <h4>Help others find inspiration and helpful tips.</h4>            
         </div>
-   
+
+        <div className='form-grid'>
+                <label>Answer Question Here:
+                    <textarea name="message" id='message' />
+                </label>
+            </div>  
         </>
     );
 }
