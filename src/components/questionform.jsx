384c00a0aa4800d86a2ba4cbea35758e35dabb10
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-12-15 16:25:43 -0800

    add code

--- src/components/questionform.jsx
+++ src/components/questionform.jsx
@@ -10,14 +10,20 @@ function Questionit() {
             <h4>Get great ideas, tips and inspiration from our Winter Wonderland community.</h4>            
         </div>
 
+
         <div className='form-grid'>
+        <div class="d-flex justify-content-center">
                 <label>Enter Question Here:
                     <textarea name="message" id='message' />
                 </label>
-            </div>
+        </div>
+        </div>
+
 
         <div className='back-button'>
+        <div class="d-flex justify-content-center">
             <button type="submit" id='button'>Back</button>
+        </div>
         </div>
 
 
