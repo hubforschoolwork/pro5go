a9f3a882e1b480019a24bb298418fee394ed3552
hubforschoolwork <feliciaj6@sbcglobal.net>
2024-12-27 14:53:21 -0800

    code edits to backend functionality

--- backend/db.js
+++ backend/db.js
@@ -3,10 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+console.log('MongoDB URI:', process.env.MONGODB_URI); // Debug line
+
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+
 db.once('open', () => {
   console.log('Connected to the database');
 });
