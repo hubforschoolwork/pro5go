71df23a2e4b1b13b069340c0ab0fa68742414a4c
hubforschoolwork <feliciaj6@sbcglobal.net>
2025-01-04 14:32:05 -0800

    Edit code

--- src/components/outdoortop.jsx
+++ src/components/outdoortop.jsx
@@ -1,34 +1,3 @@
-// import React, { useState } from 'react';
-// import ForumForm from './forumform';
-
-// function TopicPage({ topic }) {
-//   const [posts, setPosts] = useState([]);
-
-//   const handleFormSubmit = (text) => {
-//     setPosts([...posts, text]);
-//   };
-
-//   return (
-//     <div className="container">
-//       <h1>{topic}</h1>
-//       <ForumForm onSubmit={handleFormSubmit} />
-//       <div className="mt-4">
-//         {posts.map((post, index) => (
-//           <div key={index} className="card mb-2">
-//             <div className="card-body">
-//               {post}
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default TopicPage;
-
-
-
 import React, { useState } from 'react';
 import ForumForm from './forumform';
 
