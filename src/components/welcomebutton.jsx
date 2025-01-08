63276fc52175fc57ca1607bdefb3d79803c8b992
hubforschoolwork <feliciaj6@sbcglobal.net>
2025-01-08 15:28:04 -0800

    Edits to code for homepage

--- src/components/welcomebutton.jsx
+++ src/components/welcomebutton.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import './welcomebutton.css';
 
+
+
+// const Welcomebutton = () => {
+//     return (
+//         <Link to="/guestarea" className="welcome-button">
+//              <button type="submit">Enter as Guest</button>
+//         </Link>
+//     )
+// }
+
+// export default Welcomebutton;
 
 
 const Welcomebutton = () => {
     return (
-        <Link to="/guestarea" className="welcome-button">
-             <button type="submit">Enter as Guest</button>
+        <Link to="/guestarea">
+            <button type="submit" className="Welcomebutton">Enter as Guest</button>
         </Link>
-    )
-}
+    );
+};
 
 export default Welcomebutton;
+
--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -3,23 +3,6 @@ import './welcome.css';
 import Welcomebutton from '../components/welcomebutton.jsx';
 
 
-// function Welcome() {
-//   return (
-//     <>
-//       <div className="container text-center">
-//         <div className="row">
-//           <div className="col">
-//             <img src="src/assets/santa-claus-1819933_1280.jpg" alt="Santa Claus" />
-//           </div>
-//           <div className="col-6">
-//             <h5>If you like discovering all things winter, then come and experience the magic of the season!</h5>
-//           </div>
-//           <div className="col">
-//             <img src="src/assets/cocoa-1908020_1280.jpg" alt="Cocoa" />
-//           </div>
-//         </div>
-//       </div>
-
 function Welcome() {
   return (
     <>
@@ -44,39 +27,15 @@ function Welcome() {
           </div>
         </div>
       </div>
-    <div className="container" id="welcomelink">
+
+    <div className='container'>
+      <div className="col-md-6 offset-md-3">
       <Welcomebutton />
       </div>
+    </div>
     </>
   );
 } 
 
 export default Welcome;
 
-
-// function Welcome() {
-//   return (
-//     <>
-//       <div className="container-title">
-//         <div className="row">
-//           <div className="col">
-//             <h1>Winter Wonderland</h1>
-//           </div>
-//         </div>
-//       </div>
-//       <div className="container text-center">
-//         <div className="row">
-//           <div className="col">
-//             <img src="src/assets/santa-claus-1819933_1280.jpg" alt="Santa Claus" className="img-fluid custom-img-outline" style={{ width: '300px', height: '300px' }} />
-//           </div>
-//           <div className="col-6">
-//             <h5>If you like discovering all things winter, then come and experience the magic of the season!</h5>
-//           </div>
-//           <div className="col">
-//             <img src="src/assets/cocoa-1908020_1280.jpg" alt="Cocoa" className="img-fluid custom-img-outline" style={{ width: '300px', height: '300px' }} />
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
