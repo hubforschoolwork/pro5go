5709d6c987eb396b906ca473cb94223a1b555f45
hubforschoolwork <feliciaj6@sbcglobal.net>
2025-01-12 12:10:04 -0800

    new code for login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,21 +6,25 @@ function Logins() {
     return (
     <>
       
-       <div className="form-container">
-       <h1>Login</h1>
-       </div>
-
-       <div className='container-title'>
-      <div className="row">
-        <div className="col">
-          <h1>Winter Wonderland</h1>
-        </div>
+<div className='form-container'>
+  <div className='container-title'>
+    <div className="row">
+      <div className="col">
+        <h1>Winter Wonderland</h1>
       </div>
-    </div>    
+    </div>
+  </div>    
+</div>
 
-       <Loginform />          
 
 
+<div className="form-container">
+  <h1>Login</h1>
+</div>
+
+<Loginform />          
+
+{/* 
         <div className="card mb-3">
             <img src="src/assets/man-498473_1280.jpg" class="card-img-top" alt="..."/>
             <div className="card-body">
@@ -28,7 +32,7 @@ function Logins() {
             <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
             </div>
-            </div>
+            </div> */}
 
     </>
     );
