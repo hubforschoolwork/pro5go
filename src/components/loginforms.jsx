60526a1279dee2443ce9bb260cd23436828f1b04
hubforschoolwork <feliciaj6@sbcglobal.net>
2025-01-12 15:37:00 -0800

    Fix css on login page

--- src/components/loginforms.jsx
+++ src/components/loginforms.jsx
@@ -78,7 +78,7 @@ function Login() {
                 </div>
 
 
-                <div className='form-grid-button'>
+                <div className='form-grid'>
                     <button type="submit">Submit</button>
                 </div>
 
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -6,7 +6,7 @@ function Logins() {
     return (
     <>
       
-<div className='form-container'>
+{/* <div className='form-container'> */}
   <div className='container-title'>
     <div className="row">
       <div className="col">
@@ -14,25 +14,13 @@ function Logins() {
       </div>
     </div>
   </div>    
-</div>
-
-
+{/* </div> */}
 
 <div className="form-container">
   <h1>Login</h1>
 </div>
 
 <Loginform />          
-
-{/* 
-        <div className="card mb-3">
-            <img src="src/assets/man-498473_1280.jpg" class="card-img-top" alt="..."/>
-            <div className="card-body">
-            <h5 className="card-title">Outdoor Activities</h5>
-            <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p className="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-            </div>
-            </div> */}
 
     </>
     );
