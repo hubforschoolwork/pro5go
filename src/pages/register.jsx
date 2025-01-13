cad909d80fe3df7d1e22cfeb0d8b47e21e4c2b1d
hubforschoolwork <feliciaj6@sbcglobal.net>
2025-01-13 12:02:30 -0800

    css changes

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -5,14 +5,13 @@ import './register.css';
 
 function Registeruser() {
     return (
-    // <div className="form-container">
-    //         <h3>Register</h3>
+
 <>
-            <div className='container'>    
-                <div className='container-title h1'>
-                <h1>Register</h1>
-                </div>
-            </div>
+    <div className='container'>    
+        <div className='container-title h1'>
+            <h1>Register</h1>
+        </div>
+    </div>
 
     <Register />
 
@@ -22,10 +21,3 @@ function Registeruser() {
 
 export default Registeruser;
 
-
-
-// <div className='container'>    
-//   <div className='container-title h1'>
-//         <h1>Login</h1>
-//       </div>
-//       </div>
