5f2363a8361be9b8a0011d2f0b57b897953f2022
hubforschoolwork <feliciaj6@sbcglobal.net>
2025-01-20 16:40:08 -0800

    dashboard changes to css

--- src/components/dashboardmain.jsx
+++ src/components/dashboardmain.jsx
@@ -4,7 +4,7 @@ import './dashboardmain.css';
 const Dashboardmain = () => {
     return (
         <>
-        <div className="container d-flex">
+        <div className="container-dash d-flex">
 
             <div className="content p-4" style={{ marginLeft: '250px', width: '100%' }}>
                 <h1>Welcome to the Dashboard</h1>
