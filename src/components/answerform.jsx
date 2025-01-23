2f39481c44032547b930aaf8334b638bc345b74e
hubforschoolwork <feliciaj6@sbcglobal.net>
2025-01-22 16:50:35 -0800

    Edit/clean up code

--- src/components/answerform.jsx
+++ src/components/answerform.jsx
@@ -1,28 +1,3 @@
-// import React from 'react';
-// // import './cooking.css'
-
-// function Answerit() {
-//     return (
-//         <>
-//         <div className="form-container">
-//             <h2>Answer a Question</h2>
-//             <h4>Help others find inspiration and helpful tips.</h4>            
-//         </div>
-
-//         <div className='form-grid'>
-//                 <label>Answer Question Here:
-//                     <textarea name="message" id='message' />
-//                 </label>
-//             </div>  
-//         </>
-//     );
-// }
-
-// export default Answerit;
-
-
-//new jan 6
-
 import React, { useState } from 'react';
 
 const AnswerForm = ({ onSubmit }) => {
--- src/components/outdoortop.jsx
+++ src/components/outdoortop.jsx
@@ -1,37 +1,3 @@
-// import React, { useState } from 'react';
-// import ForumForm from './forumform';
-
-// function TopicPage({ topic }) {
-//   const [posts, setPosts] = useState([]);
-
-//   const handleFormSubmit = (text) => {
-//     const newPost = {
-//       text,
-//       timestamp: new Date().toLocaleString() // Capture the current date and time
-//     };
-//     setPosts([...posts, newPost]);
-//   };
-
-//   return (
-//     <div className="container">
-//       <h1>{topic}</h1>
-//       <ForumForm onSubmit={handleFormSubmit} />
-//       <div className="mt-4">
-//         {posts.map((post, index) => (
-//           <div key={index} className="card mb-2">
-//             <div className="card-body">
-//               <p>{post.text}?</p>
-//               <small className="text-muted">Submitted on: {post.timestamp}</small>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
-
-// export default TopicPage;
-
 import React, { useState } from 'react';
 import ForumForm from './forumform';
 import AnswerForm from './answerform';
